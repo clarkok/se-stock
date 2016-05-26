96ae14edc7bef51332fbbd6c6ef9069de9368a95
Clark Zhang <mail@clarkok.com>
2016-05-26 20:05:56 +0800

    change stock to string

--- test/database/database_test.js
+++ test/database/database_test.js
@@ -9,30 +9,30 @@ describe('Database', function () {
 
     beforeEach(function () {
         return uut.clearAll()
-            .then(() => uut.commitBuyingInstruction(1, 12345, 10, 100))
-            .then(() => uut.commitSellingInstruction(2, 12345, 20, 200));
+            .then(() => uut.commitBuyingInstruction(1, '12345', 10, 100))
+            .then(() => uut.commitSellingInstruction(2, '12345', 20, 200));
     });
 
     describe('#commitBuyingInstruction', function () {
         it('commit a buying instruction', function () {
-            return uut.commitBuyingInstruction(1, 12345, 10, 10).then((ids) => { assert.typeOf(ids, 'number'); });
+            return uut.commitBuyingInstruction(1, '12345', 10, 10).then((ids) => { assert.typeOf(ids, 'number'); });
         });
     });
 
     describe('#commitSellingInstruction', function () {
         it('commit a selling instruction', function () {
-            return uut.commitSellingInstruction(2, 12345, 20, 20).then((ids) => { assert.typeOf(ids, 'number'); });
+            return uut.commitSellingInstruction(2, '12345', 20, 20).then((ids) => { assert.typeOf(ids, 'number'); });
         });
     });
 
     describe('#getAllSellingInstuctions', function () {
         it('should list all selling instructions', function () {
-            return uut.getAllSellingInstructions(12345)
+            return uut.getAllSellingInstructions('12345')
                 .then((insts) => {
                     assert.equal(insts.length, 1);
                     assert.equal(insts[0].is_buying, 0);
                     assert.equal(insts[0].owner, 2);
-                    assert.equal(insts[0].stock, 12345);
+                    assert.equal(insts[0].stock, '12345');
                     assert.equal(insts[0].amount, 200);
                     assert.equal(insts[0].price, 20);
                 });
@@ -41,12 +41,12 @@ describe('Database', function () {
 
     describe('#getAllBuyingInstuctions', function () {
         it('should list all buying instructions', function () {
-            return uut.getAllBuyingInstructions(12345)
+            return uut.getAllBuyingInstructions('12345')
                 .then((insts) => {
                     assert.equal(insts.length, 1);
                     assert.equal(insts[0].is_buying, 1);
                     assert.equal(insts[0].owner, 1);
-                    assert.equal(insts[0].stock, 12345);
+                    assert.equal(insts[0].stock, '12345');
                     assert.equal(insts[0].amount, 100);
                     assert.equal(insts[0].price, 10);
                 });
@@ -56,22 +56,22 @@ describe('Database', function () {
     describe('#makeTrade', function () {
         it('should make a trade', function () {
             let buying_id, selling_id;
-            return uut.getAllBuyingInstructions(12345)
+            return uut.getAllBuyingInstructions('12345')
                 .then((insts) => {
                     assert.equal(insts.length, 1);
                     buying_id = insts[0].id;
-                    return uut.getAllSellingInstructions(12345);
+                    return uut.getAllSellingInstructions('12345');
                 })
                 .then((insts) => {
                     assert.equal(insts.length, 1);
                     selling_id = insts[0].id;
                     return uut.makeTrade(buying_id, selling_id, 50, 100);
                 })
-                .then(() => uut.getAllBuyingInstructions(12345))
+                .then(() => uut.getAllBuyingInstructions('12345'))
                 .then((insts) => {
                     assert.equal(insts.length, 1);
                     assert.equal(insts[0].amount, 50);
-                    return uut.getAllSellingInstructions(12345);
+                    return uut.getAllSellingInstructions('12345');
                 })
                 .then((insts) => {
                     assert.equal(insts.length, 1);
@@ -83,24 +83,24 @@ describe('Database', function () {
     describe('#queryLastOperation', function () {
         it('should get last operation', function () {
             let buying_id, selling_id;
-            return uut.getAllBuyingInstructions(12345)
+            return uut.getAllBuyingInstructions('12345')
                 .then((insts) => {
                     assert.equal(insts.length, 1);
                     buying_id = insts[0].id;
-                    return uut.getAllSellingInstructions(12345);
+                    return uut.getAllSellingInstructions('12345');
                 })
                 .then((insts) => {
                     assert.equal(insts.length, 1);
                     selling_id = insts[0].id;
                     return uut.makeTrade(buying_id, selling_id, 50, 100);
                 })
-                .then(() => uut.queryLastOperation(12345))
+                .then(() => uut.queryLastOperation('12345'))
                 .then((op) => {
                     assert.equal(op.buying_id, buying_id);
                     assert.equal(op.selling_id, selling_id);
                     assert.equal(op.amount, 50);
                     assert.equal(op.price, 100);
-                    assert.equal(op.stock, 12345);
+                    assert.equal(op.stock, '12345');
                 });
         });
     });
@@ -108,7 +108,7 @@ describe('Database', function () {
     describe('#cancelInstruction', function () {
         it('should cancel an instruction', function () {
             let buying_id;
-            return uut.getAllBuyingInstructions(12345)
+            return uut.getAllBuyingInstructions('12345')
                 .then((insts) => {
                     assert.equal(insts.length, 1);
                     buying_id = insts[0].id;
